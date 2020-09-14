eb69933adb12c55a65791462d4cef7991127c3fd
VWO53 <64356181+VWO53@users.noreply.github.com>
2020-09-14 14:33:17 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,63 +1,74 @@
 class Translation(object):
-    START_TEXT = """Hello,
-This is a Telegram URL Upload Bot made by @prgofficial
+    START_TEXT = """
+**Hello, This is YouTube Downloader  Bot**
+      
+```I Will Convert Youtube Link to Video/File & Mp3```
 
-<b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
+**Do one By One Otherwise you will get Permenent Ban**
+        
+/help for more details...""" 
 
-/help for more details..
-
-Support Group : @moviesonlydiscussion
-© @prgofficial"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
+    FORMAT_SELECTION = "Current Thumbnail: <a href='{}'>Thumbnail</a> \n\nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons"
+    SET_CUSTOM_USERNAME_PASSWORD =" "
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = "**📥 DOWNLOADING 📥**"
+    UPLOAD_START = "**📤 UPLOADING 📤**"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @moviesonlydiscussion"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @moviesonlydiscussion \nUploaded in {} seconds."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.Detected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @TG_BotZ"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS ="""```Downloaded in {} seconds.\nUploaded in {} seconds.\n```
+
+Channel:- © @TG_BotZ"""
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = " "
+    CUSTOM_CAPTION_UL_FILE = "© @TG_BotZ "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    CURENT_PLAN_DETAILS = """Current plan details
---------
-Telegram ID: <code>{}</code>
-Plan name: Free User
-Expires on: 31/12/2020"""
-    HELP_USER = """Hai am URL Uploader bot..
-    
-1. Send url (Link|New Name with Extension).
-2. Send Custom Thumbnail (Optional).
-3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
-   
-<b>👉 Join For New Movies :</b> 👉 <a href="https://t.me/joinchat/AAAAAEw1pO1bBX6ylAb0hQ">JOIN</a>
+    HELP_USER ="""**Hai Follow These steps**
 
---------
-Send /me to know current plan details
+**>>>**
+```Send YouTube url | New Name```
 
-Support Group : @MoviEzoNlysuppOrT
-© @MovieZonlYheRe"""
+**>>>**
+```Send custom Thumbnail (Optional). Link Thumbnail will add automatically and you can find thumbnail above the Buttons.``` 
+```If you don't need thumbnail hit /deletethumbnail```
+
+**>>>**
+```Select the Button which Media you want..```
+
+"""
+    ABOUT_USER ="""
+○ **My Name** : You Tube Downloader
+
+○ **Creator** : <a href="https://t.me/lNS4N3">lNS4N3</a> 
+
+○ **Credits** : ```Everyone in this journey```
+
+○ **Language** : Python3
+
+○ **Library** : <a href="https://docs.pyrogram.org/">Pyrogram asyncio 0.16.1</a> 
+
+○ **Source Code** : <a href="https://t.me/nokkiirunnoippokittum">👉 Click Here</a> 
+
+○ **Server** : <a href="https://herokuapp.com/">Heroku</a> 
+
+○ **Build Status** : v2.1[Beta]"""
+    PLAN_TEXT = """**Hai Your Plan Details**
+```Plan name: Free User```
+```Expires on: Until my Death 😍```"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
-    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AnyDLBot"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @TG_BotZ"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
     FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
